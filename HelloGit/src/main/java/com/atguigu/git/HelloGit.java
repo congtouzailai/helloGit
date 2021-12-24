4a438f3a3fec04f7d648f2f4044c76c79efb233c
shaoyuhang <18851092136@163.com>
2021-12-24 14:50:27 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;/**
+ * @author shaoyuhang
+ * @date 2021/12/24-14:44
+ * @version version 1.0
+ */
+
+/**
+ * @author shaoyuhang
+ * @date 2021/12/24-14:44
+ * @version version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
